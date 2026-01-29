e9d611379a04565c91c8ccbb656249a7c3eddbb2
portfolio-webprofile <portfolio.webprofile@gmail.com>
2026-01-29 09:05:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const loading = document.getElementById("loadingScreen");
 
-// 🔥 FORCE REMOVE LOADING SCREEN
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    if (loading) {
-      loading.style.display = "none";
-    }
-  }, 800); // GTA-style delay
-});
